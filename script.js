9425fca14eddcd0f9d7355b19a790b4c5913289a
Youngin Choi <105949254+soysauce4444@users.noreply.github.com>
2023-03-03 17:49:13 -0500

    Add game function, accept user input Create variable for computerSelection and for user input (convert to lowercase). Create function game to compare computerSelection and playerSelection and return wins or loses.

--- script.js
+++ script.js
@@ -16,8 +16,44 @@ function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+
+function game(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    } 
+    else if (playerSelection == rock, computerSelection == paper) {
+        return "You lose, paper beats rock."
+    } 
+    else if (playerSelection == paper, computerSelection == scissors) {
+        return "You lose, scissors beats paper."
+    } 
+    else if (playerSelection == scissors, computerSelection == rock) {
+        return "You lose, rock beats scissors."
+    } 
+    else if (playerSelection == rock, computerSelection == scissors) {
+        return "You win, rock beats scissors!"
+    } 
+    else if (playerSelection == rock, computerSelection == scissors) {
+        return "You win, rock beats scissors!"
+    } 
+    else if (playerSelection == rock, computerSelection == scissors) {
+        return "You win, rock beats scissors!"
+    } 
+    else if (playerSelection == rock, computerSelection == scissors) {
+        return "You win, rock beats scissors!"
+    } 
+    else {
+        return "WIP"
+    }
+}
+
+
 randomInt = getRandomInt(1, 3);
+computerSelection = getComputerChoice(randomInt);
+playerSelection = window.prompt("What's your choice?").toLowerCase();
+
+console.log(computerSelection);
+console.log(playerSelection);
+console.log(game(playerSelection, computerSelection));
 
 
-console.log(randomInt)
-console.log(getComputerChoice(randomInt))
