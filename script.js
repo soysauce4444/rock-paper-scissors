14ffb7539b73ad99f5ba973847f9c2c7bf60e28c
Youngin Choi <105949254+soysauce4444@users.noreply.github.com>
2023-06-16 15:19:58 -0400

    Add keepscore function & return to game

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
+// RNG
 function randInt (min, max) {
     return Math.floor (Math.random() * (max - min + 1) + min);
 }
 
-function playRound (getComputerChoice, playerChoice) {
-    getComputerChoice  = 
-        (randInt(1,3) == 1) ? getComputerChoice = "rock" :
-        (randInt(1,3) == 2) ? getComputerChoice = "paper" :
+// keep score
+function getScore (ComputerChoice, playerChoice) {
+    return (ComputerChoice == playerChoice) ? "No change in score" :
+
+           (ComputerChoice == "rock" && playerChoice == "scissors" ||
+           ComputerChoice == "paper" && playerChoice == "rock" ||
+           ComputerChoice == "scissors" && playerChoice == "paper") ? computerScore++:
+
+           playerScore++;
+}
+
+// report winner or loser
+
+
+function playRound (ComputerChoice, playerChoice) {
+    /*  enable for single rounds
+    ComputerChoice  = 
+        (randInt(1,3) == 1) ? ComputerChoice = "rock" :
+        (randInt(1,3) == 2) ? ComputerChoice = "paper" :
         "scissors";
     var playerChoice = prompt("Choose rock, paper, or scissors: ").toLowerCase();
-    console.log("I chose: ", getComputerChoice);
+    */
+    console.log("I chose: ", ComputerChoice);
     console.log("You chose: ", playerChoice);
-    return (getComputerChoice == playerChoice) ? "It's a tie." :
+    return (ComputerChoice == playerChoice) ? "It's a tie." :
 
-           (getComputerChoice == "rock" && playerChoice == "scissors" ||
-           getComputerChoice == "paper" && playerChoice == "rock" ||
-           getComputerChoice == "scissors" && playerChoice == "paper") ? "I win!" :
+           (ComputerChoice == "rock" && playerChoice == "scissors" ||
+           ComputerChoice == "paper" && playerChoice == "rock" ||
+           ComputerChoice == "scissors" && playerChoice == "paper") ? "I win!":
 
            "I lose T_T";
 }
 
 function game() {
-    for (i = 0; i < 5; i++) {
-        /*
-        var getComputerChoice = 
-            (randInt(1,3) == 1) ? getComputerChoice = "rock" :
-            (randInt(1,3) == 2) ? getComputerChoice = "paper" :
+    for (i = 0; i < 2; i++) {
+        
+        var ComputerChoice = 
+            (randInt(1,3) == 1) ? ComputerChoice = "rock" :
+            (randInt(1,3) == 2) ? ComputerChoice = "paper" :
             "scissors";
         var playerChoice = prompt("Choose rock, paper, or scissors: ").toLowerCase();
-*/
-        playRound(getComputerChoice, playerChoice);
+
+        getScore(ComputerChoice, playerChoice);
+
+        console.log(playRound(ComputerChoice, playerChoice));
     }
+    return ("Your score: " + playerScore + "\nMy score: " + computerScore);
 }
 
-var getComputerChoice;
+var ComputerChoice;
 var playerChoice;
+
+var computerScore = 0;
+var playerScore = 0;
 
 console.log(game());
