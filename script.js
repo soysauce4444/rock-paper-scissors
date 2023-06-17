86be56c0a4119ec8c1d3ccabc8f3cf63b52e44e0
Youngin Choi <105949254+soysauce4444@users.noreply.github.com>
2023-06-17 14:50:41 -0400

    Add buttons for choices and eventlisteners

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    console.log(playRound(ComputerChoice, 'rock'));
+})
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    console.log(playRound(ComputerChoice, 'paper'));
+})
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    console.log(playRound(ComputerChoice, 'scissors'));
+})
+
 // RNG
 function randInt (min, max) {
     return Math.floor (Math.random() * (max - min + 1) + min);
@@ -22,29 +37,24 @@ function getWinner (computerScore, playerScore) {
 }
 
 function playRound (ComputerChoice, playerChoice) {
-    /*  enable for single rounds
+    //  enable for single rounds
     ComputerChoice  = 
         (randInt(1,3) == 1) ? ComputerChoice = "rock" :
         (randInt(1,3) == 2) ? ComputerChoice = "paper" :
         "scissors";
-    var playerChoice = prompt("Choose rock, paper, or scissors: ").toLowerCase();
+    // var playerChoice = prompt("Choose rock, paper, or scissors: ").toLowerCase();
     
-    console.log("I chose: ", ComputerChoice);
-    console.log("You chose: ", playerChoice);
-    */
-    return  "I chose: " + ComputerChoice + "\nYou chose: " + playerChoice;
+    console.log("I chose: " + ComputerChoice + "\nYou chose: " + playerChoice);
     
-    /*
-    (ComputerChoice == playerChoice) ? "It's a tie." :
+    return (ComputerChoice == playerChoice) ? "It's a tie." :
 
            (ComputerChoice == "rock" && playerChoice == "scissors" ||
            ComputerChoice == "paper" && playerChoice == "rock" ||
            ComputerChoice == "scissors" && playerChoice == "paper") ? "I win!":
 
            "I lose T_T";
-           */
 }
-
+/*
 function game() {
     for (i = 0; i < 2; i++) {
         
@@ -61,11 +71,11 @@ function game() {
     console.log("\n" + getWinner(computerScore, playerScore));
     return ("Your score: " + playerScore + "\nMy score: " + computerScore);
 }
-    
+*/
 var ComputerChoice;
 var playerChoice;
 
 var computerScore = 0;
 var playerScore = 0;
 
-console.log(game());
+console.log(playRound(ComputerChoice, playerChoice));
